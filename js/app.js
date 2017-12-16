cbced617512d7558a71cafbc69a8b854e6665154
Lukas Romsicki <lukas.romsicki@shopify.com>
2017-12-16 15:06:49 -0500

    Refactor CSS

--- js/app.js
+++ js/app.js
@@ -13,31 +13,10 @@ $(document).ready(function() {
         }
     });
 
-    initOnLoad();
+    updateLeftLabelPosition();
 
     $(window).scroll(function() {
-        var wrapper = $(".wrapper");
-        var wrapperHeight = wrapper.height();
-        var sections = wrapper.find('section');
-        var pageHeight = $(this).height();
-        var pageTop = $(this).scrollTop();
-
-        $.each($(".page-data"), function() {
-            var bottom = pageTop + pageHeight;
-            var sectionTop = $(this).offset().top;
-            var sectionBottom = sectionTop + $(this).outerHeight();
-            var sectionTitle = $(this).find('.section-title');
-
-            if (pageTop >= sectionTop && bottom < sectionBottom && !sectionTitle.hasClass('fixed')) {
-                sectionTitle.addClass('fixed').removeClass('bottom');
-
-            } else if (sectionTitle.hasClass('fixed') && bottom > sectionBottom) {
-                sectionTitle.addClass('bottom').removeClass('fixed');
-
-            } else if (sectionTitle.hasClass('fixed') && pageTop < sectionTop) {
-                sectionTitle.removeClass('bottom').removeClass('fixed');
-            }
-        });
+        updateLeftLabelPosition();
 
         $('nav a').each(function () {
             var currLink = $(this);
@@ -53,7 +32,7 @@ $(document).ready(function() {
     });
 
     $(window).resize(function() {
-        initOnLoad();
+        updateLeftLabelPosition();
     });
 });
 
@@ -69,9 +48,27 @@ function removeMenuActiveClasses() {
     $(".slide-menu").addClass("hidden");
 }
 
-function initOnLoad() {
+function updateLeftLabelPosition() {
     var wrapper = $(".wrapper");
     var wrapperHeight = wrapper.height();
     var sections = wrapper.find('section');
-    var pageHeight = sections.height();
+    var pageHeight = $(this).height();
+    var pageTop = $(this).scrollTop();
+
+    $.each($(".page-data"), function() {
+        var bottom = pageTop + pageHeight;
+        var sectionTop = $(this).offset().top;
+        var sectionBottom = sectionTop + $(this).outerHeight();
+        var sectionTitle = $(this).find('.section-title');
+
+        if (pageTop >= sectionTop && bottom < sectionBottom && !sectionTitle.hasClass('fixed')) {
+            sectionTitle.addClass('fixed').removeClass('bottom');
+
+        } else if (sectionTitle.hasClass('fixed') && bottom > sectionBottom) {
+            sectionTitle.addClass('bottom').removeClass('fixed');
+
+        } else if (sectionTitle.hasClass('fixed') && pageTop < sectionTop) {
+            sectionTitle.removeClass('bottom').removeClass('fixed');
+        }
+    });
 }
