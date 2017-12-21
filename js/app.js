fa4735140b447fc6a90dc595ff1ac74760e3ed8c
Lukas Romsicki <lukas.romsicki@shopify.com>
2017-12-21 18:57:36 -0500

    Enhance images, remove unused scripts

--- js/app.js
+++ js/app.js
@@ -7,23 +7,11 @@ $(document).ready(function() {
         }
     });
 
-    /*
-    $("section").click(function() {
-        if ($(".hamburger").hasClass("is-active")) {
-            removeMenuActiveClasses();
-        }
-    });
-    */
-
-    //updateSectionTitle();
-
     $(window).scroll(function() {
-        //updateSectionTitle();
         updateActiveMenuItem();
     });
 
     $(window).resize(function() {
-        //updateSectionTitle();
         updateActiveMenuItem();
     });
 });
@@ -38,31 +26,6 @@ function removeMenuActiveClasses() {
     $(".hamburger").removeClass("is-active");
     $(".menu-button").removeClass("is-active");
     $(".slide-menu").addClass("hidden");
-}
-
-function updateSectionTitle() {
-    var wrapper = $(".wrapper");
-    var wrapperHeight = wrapper.height();
-    var sections = wrapper.find('section');
-    var pageHeight = $(this).height();
-    var pageTop = $(this).scrollTop();
-
-    $.each($("section"), function() {
-        var bottom = pageTop + pageHeight;
-        var sectionTop = $(this).offset().top;
-        var sectionBottom = sectionTop + $(this).outerHeight();
-        var sectionTitle = $(this).find('.section-title');
-
-        if (pageTop >= sectionTop && bottom < sectionBottom && !sectionTitle.hasClass('fixed')) {
-            sectionTitle.addClass('fixed').removeClass('bottom');
-
-        } else if (sectionTitle.hasClass('fixed') && bottom > sectionBottom) {
-            sectionTitle.addClass('bottom').removeClass('fixed');
-
-        } else if (sectionTitle.hasClass('fixed') && pageTop < sectionTop) {
-            sectionTitle.removeClass('bottom').removeClass('fixed');
-        }
-    });
 }
 
 function updateActiveMenuItem() {
